0c537f90510f3a271a27f1f013b0cc0fe3d9735d
Danmaro <danmaro@OZHEN>
2025-06-02 18:02:15 -0600

    mod - aviso removido

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('aviso');
-});
-
-Route::get('/buscar', [MainController::class, 'buscar'])->name('buscar');
+Route::get('/', [MainController::class, 'buscar'])->name('buscar');
 Route::get('/resultados', [MainController::class, 'busqueda'])->name('resultados');
 Route::get('/cni', [MainController::class, 'cni'])->name('cni');
